2d00f880cb834f47466cb9d26edab3a38719b731
JackHamby <jackhamby@JackHambys-MBP.lan>
2020-11-04 22:03:47 -0600

    wrap up tables

--- docker-entrypoint-initdb.d/init.sql
+++ docker-entrypoint-initdb.d/init.sql
@@ -1,3 +1,47 @@
+
+create table users (
+    user_id int generated always as identity,
+    first_name text not null,
+    last_name text not null,
+    email text not null,
+    phone text not null,
+
+    primary key (user_id)
+);
+
+create table contacts (
+    contact_id int generated always as identity,
+    first_name text not null,
+    last_name text,
+    email text,
+    phone text,
+
+    primary key (contact_id)
+);
+
+
+create table locations (
+    location_id int generated always as identity,
+    latitude int not null,
+    longitude int not null,
+    line1 text,
+    line2 text,
+    zip text not null,
+    city text not null,
+    state text not null,
+    contact_id int not null,
+    user_id int not null,
+
+
+    primary key (location_id),
+    constraint users
+        foreign key (user_id)
+            references users (user_id),
+    constraint contacts
+        foreign key (contact_id)
+            references contacts (contact_id)
+);
+
 create table events (
     event_id int generated always as identity,
     name text not null,
@@ -9,22 +53,8 @@ create table events (
     cost decimal not null,
     user_id int not null,
 
-    primary key (eventId),
-    constraint fk_user
+    primary key (event_id),
+    constraint users
         foreign key (user_id)
-            references user(user_id) 
+            references users (user_id) 
 );
-
-
-create table user {
-    user_id int generated always as identity,
-    first_name text not null,
-    last_name text not null,
-    email text not null,
-    phone text not null,
-
-    primary key (userId)
-}
-
-
-c
